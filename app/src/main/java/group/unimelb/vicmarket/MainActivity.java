c45e9a3d68fd6c29e28ece46bbd16fb76ad10fcd
JosephVuoto <joseph.vuoto@gmail.com>
2020-08-31 16:46:37 +1000

    initialize the project

---
+++ app/src/main/java/group/unimelb/vicmarket/MainActivity.java
@@ -0,0 +1,14 @@
+package group.unimelb.vicmarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
